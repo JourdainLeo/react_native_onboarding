c82c026cd6648db89f97a9aa471a76c4905845b4
Léo Jourdain <leo@jourdain.io>
2025-09-08 16:25:22 +0200

    feat: add red square

---
+++ MyNewApp/components/RedSquare.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const RedSquare: React.FC = () => (
+    <View style={styles.square}>
+        <Text style={styles.text}>Hello</Text>
+    </View>
+);
+
+const styles = StyleSheet.create({
+    square: {
+        width: 100,
+        height: 100,
+        backgroundColor: 'red',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        color: 'green',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+});
+
+export default RedSquare;
